faa4e3ef9c0e9d233191dc883bfda5eb3846bc53
Hinako-Tsutsumi <1950613h@stu.kobe-u.ac.jp>
2020-10-25 15:06:35 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,5 +2,14 @@
 function setup(){
   for(let i = 0; i < 10; i++){
     // BLANK[1]
-  }
+ noFill();
+ if(i < 5){
+ stroke(0, 0, 255);
+ }
+ else{
+ stroke(255, 0, 0);
+ }
+let x = 5 + i * 10;
+ellipse(50, 50, x);
+}
 }
