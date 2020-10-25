62355be2836c019fb32c5255407ff3b7df561dcc
Hinako-Tsutsumi <1950613h@stu.kobe-u.ac.jp>
2020-10-25 18:37:14 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,35 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      if(i % 2 == 0 && j % 2 == 1){
+        fill(128);
+        rect(size * i, size * j, size, size);
+        if(j == 1){
+          fill(255, 0, 0)
+          ellipse(size * i + size / 2, size * j + size / 2, size)
+        }
+        else if(j == 5 || j == 7){
+          fill(0)
+          ellipse(size * i + size / 2, size * j + size / 2, size)
+        }
+      }
+      else if(i % 2 == 1 && j % 2 == 0){
+        fill(128);
+        rect(size * i, size * j, size, size);
+        if(j == 0 || j == 2){
+          fill(255, 0, 0)
+          ellipse(size * i + size / 2, size * j + size / 2, size)
+        }
+        else if(j == 6){
+          fill(0)
+          ellipse(size * i + size / 2, size * j + size / 2, size)
+        }
+      }
+      else{
+        fill(255)
+        rect(size * i, size * j, size, size);
+      }
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
-  }
+}
 }
