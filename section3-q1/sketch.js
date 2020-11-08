5e0dea49cb1c8d49b9a456f65dace101f2614d78
Hinako-Tsutsumi <1950613h@stu.kobe-u.ac.jp>
2020-11-09 00:29:54 +0900

    change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,20 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 30, 30, 128, 0, 128, 255, 255, 255);
 }
 
-function balloon(t){
+function balloon(t, x, y, bcR, bcG, bcB, tcR, tcG, tcB){
+  push();
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  noStroke();
+  fill(bcR, bcG, bcB);
+  rect(x, y, w + p * 2, h + p * 2);
+  fill(tcR, tcG, tcB);
+  text(t, x + p, y + h + p);
+  fill(bcR, bcG, bcB);
+  triangle(x + p + 10, y + h + p * 2, x + p + 30, y + h + p * 2, x + p, y + h + p * 2 + 30);
+  pop();
 }
