f26145721809e65f52f4313b1d14c77e4f07f0cb
Hinako-Tsutsumi <1950613h@stu.kobe-u.ac.jp>
2020-11-12 11:52:32 +0900

    Update section 2-q2
    再提出です。

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,26 +5,14 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      if(i % 2 == 0 && j % 2 == 1){
+      if(i % 2 != j % 2){
         fill(128);
         rect(size * i, size * j, size, size);
-        if(j == 1){
+        if(j == 0 || j == 1 || j == 2){
           fill(255, 0, 0)
           ellipse(size * i + size / 2, size * j + size / 2, size)
         }
-        else if(j == 5 || j == 7){
-          fill(0)
-          ellipse(size * i + size / 2, size * j + size / 2, size)
-        }
-      }
-      else if(i % 2 == 1 && j % 2 == 0){
-        fill(128);
-        rect(size * i, size * j, size, size);
-        if(j == 0 || j == 2){
-          fill(255, 0, 0)
-          ellipse(size * i + size / 2, size * j + size / 2, size)
-        }
-        else if(j == 6){
+        else if(j == 5 || j == 6 || j == 7){
           fill(0)
           ellipse(size * i + size / 2, size * j + size / 2, size)
         }
