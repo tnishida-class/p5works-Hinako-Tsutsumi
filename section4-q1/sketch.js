9391d38a2da52add36df42170dcdb9b0f8796d97
Hinako-Tsutsumi <1950613h@stu.kobe-u.ac.jp>
2020-11-17 17:47:01 +0900

    Update 4-q1
    必須課題　再提出

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,19 +1,23 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  size = 30;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  size = count;
-  if(count > 50){
-    count = -1 * count;
+  if(count < 50){
+    size += 1;
+  }
+  else{
+    size -= 1;
   }
    // BLANK[1]
   ellipse(width / 2, height / 2, size);
