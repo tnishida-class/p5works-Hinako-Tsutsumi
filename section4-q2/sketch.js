33e2811e1825cdc94020a9a9e72e47f664202109
Hinako-Tsutsumi <1950613h@stu.kobe-u.ac.jp>
2020-11-20 21:31:02 +0900

    Update 4-q2
    練習問題

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -16,19 +16,28 @@ function draw(){
   star(x, y, 20, angle);
   x += vx;
   y += vy;
-  // BLANK[2] (hint: 作った star 関数を使います)
+  angle += 0.1;
+  // BLANK[2] (hint: 作った star 関数を使います
 
-angle += 0.1;
+//   // 端の処理パターン (1) 反対側から出てくる
+  // if(x > width){ x = 0; }
+  // else if(x < 0){ x = width; }
+  // if(y > height){ y = 0; }
+  // if(y < 0){ y = height; }
 
-  // 端の処理パターン (1) 反対側から出てくる
-  if(x > width){ x = 0; }
-  else if(x < 0){ x = width; }
-  if(y > height){ y = 0; }
-  if(y < 0){ y = height; }
+  if(x < 0 || x > width){ vx = -1 * vx; } // 横
+  if(y < 0 || y > height){ vy = -1 * vy; } // 縦
+}
+
+function mouseClicked(){
+  x = mouseX;
+  y = mouseY;
 }
 
 function star(cx, cy, r, angle){
   beginShape();
+  noStroke();
+  fill(255, 255, 0)
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
